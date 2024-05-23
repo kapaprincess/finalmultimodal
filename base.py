7f9824f69b57bda1a658dbef7a076d7c5e9bebb9
kapaprincess <tareaskarolp@gmail.com>
2024-05-22 21:54:04 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,2 +1,6 @@
 import streamlit as st
-st.title("Primera página")
+st.title("WELCOME TO YOUR INTERACTIVE HOME")
+st.header("Instructions")
+st.text("This is the welcome page of your home. Use the emergent window on the left side to toggle between the different rooms avaliable. Inside of each yoom you will be able to control the lights and the door using your voice or the buttons.")
+st.header("Have fun!")
+
