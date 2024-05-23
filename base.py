d9d18494f43bbe2afc618bb180514250b19efb49
kapaprincess <tareaskarolp@gmail.com>
2024-05-22 22:11:06 -0500

    Update base.py

--- base.py
+++ base.py
@@ -4,5 +4,4 @@ st.header("Instructions")
 st.write("This is the welcome page of your home. Use the emergent window on the left side to toggle between the different rooms avaliable. Inside of each yoom you will be able to control the lights and the door using your voice or the buttons.")
 st.header("Have fun!")
 
-image = Image.open('room1.jpg')
-st.image(image)
+
