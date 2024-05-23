f54e732366c5e1d9f01676a1c33e8d9e1d219d58
kapaprincess <tareaskarolp@gmail.com>
2024-05-22 22:35:55 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.title("WELCOME TO YOUR INTERACTIVE HOME")
+image = Image.open('welcomehome.jpg')
+st.image(image)
 st.header("Instructions")
 st.write("This is the welcome page of your home. Use the emergent window on the left side to toggle between the different rooms avaliable. Inside of each yoom you will be able to control the lights and the door using your voice or the buttons.")
 st.header("Have fun!")
